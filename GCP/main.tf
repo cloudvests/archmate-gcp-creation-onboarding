a87d82e2668813f9a81dbdcf9634f7871c640240
mohammadrawabdeh <mohammad.rawabdeh@cloudvests.com>
2025-11-10 10:05:54 +0300

    Update main.tf

--- GCP/main.tf
+++ GCP/main.tf
@@ -40,7 +40,7 @@ data "google_client_config" "current" {}
 
 # 1️⃣ Create a GCP Service Account (Read-only Access)
 resource "google_service_account" "aws_readonly_sa" {
-  account_id   = "aws-readonly-sasasaa54"
+  account_id   = "pws-readonly-sasasaa589"
   display_name = "AWS Read-only Access Service Account"
 }
 
@@ -53,7 +53,7 @@ resource "google_project_iam_member" "readonly_binding" {
 
 # 3️⃣ Create a Workload Identity Pool
 resource "google_iam_workload_identity_pool" "aws_pool" {
-  workload_identity_pool_id = "aws-pool-alisssss54"
+  workload_identity_pool_id = "aws-pool-alisssss589"
   display_name              = "AWS Workload Identity Pool"
   description               = "Pool to allow AWS access to GCP"
   # Note: optionally specify location = "global" (default) etc.
@@ -96,12 +96,6 @@ resource "google_service_account_iam_binding" "token_creator" {
   members = [
     "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.aws_pool.name}/*"
   ]
-}
-
-resource "google_service_account_iam_member" "cloud_functions_key_admin" {
-  service_account_id = google_service_account.aws_readonly_sa.name
-  role               = "roles/iam.serviceAccountKeyAdmin"
-  member             = "serviceAccount:${local.cloud_functions_service_agent}"
 }
 
 # --------------------------------------------------------------------------------------------------

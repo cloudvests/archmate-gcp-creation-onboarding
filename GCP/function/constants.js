ccb8fee2af8ff3431737c6de83085f35606a6326
YazeedMwafi <yazeed.mowfi@cloudvests.com>
2025-11-20 17:18:11 +0300

    Restructure

---
+++ GCP/function/constants.js
@@ -0,0 +1,21 @@
+// Configuration Constants
+const ENVIRONMENT = 'dev';
+
+// Cognito Configuration
+const COGNITO_TOKEN_URL = 'https://eu-central-1yxgmmtmcl.auth.eu-central-1.amazoncognito.com/oauth2/token';
+const COGNITO_CLIENT_ID = '279kthrmc1kbopa1j95tlkf3gq';
+const COGNITO_CLIENT_SECRET_B64 = 'bq59uldpgve563hhefrgptq3k7mfml83tkk2eoqm1qpfpfn4jml';
+
+// AWS Endpoint
+const AWS_ENDPOINT = 'https://zspu86b2d7.execute-api.eu-central-1.amazonaws.com';
+const AWS_ENDPOINT_PATH = '/dev/run-assessment';
+
+module.exports = {
+  ENVIRONMENT,
+  COGNITO_TOKEN_URL,
+  COGNITO_CLIENT_ID,
+  COGNITO_CLIENT_SECRET_B64,
+  AWS_ENDPOINT,
+  AWS_ENDPOINT_PATH
+};
+
